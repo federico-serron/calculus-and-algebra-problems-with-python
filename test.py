606c9e8719766477f6266a0a740ae46d398709db
Federico Serron Umpierrez <f.serron.umpierrez@gmail.com>
2022-05-23 23:45:17 +0000

    Excercices calculus done

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-numeros = [1, 2, 34, 86, 4, 5, 99, 890, 45]
-pares = [x for x in numeros if x%2 == 0]
-print(pares)
+apellidos = ['cavani', 'suarez', 'valverde', 'torreira','vecino','gimenez']
+capitalized = [lastname.capitalize() for lastname in apellidos]
+print(capitalized)
